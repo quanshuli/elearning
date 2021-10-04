90bc8f5e6d108509050b627652766cd228659646
spadeninja <quanshuli.dev@gmail.com>
2021-10-05 08:11:33 +1000

    Update views.py

--- courses/api/views.py
+++ courses/api/views.py
@@ -4,13 +4,24 @@ from rest_framework.views import APIView
 from rest_framework.response import Response 
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action, authentication_classes, permission_classes
 from ..models import Subject, Course
 from .serializers import SubjectSerializer, CourseSerializer
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    
+
+    @action(detail=True,
+            methods=['post'],
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated]
+            )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+
 class CourseEnrollView(APIView):
     authentication_classes = (BasicAuthentication,)
     permission_classes = (IsAuthenticated,)
