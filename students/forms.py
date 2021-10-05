c30f4e1c8cd7cc72eaa3216dba2f39c24f5d058d
spadeninja <quanshuli.dev@gmail.com>
2021-10-06 08:54:56 +1000

    student

--- students/forms.py
+++ students/forms.py
@@ -2,4 +2,5 @@ from django import forms
 from courses.models import Course
 
 class CourseEnrollForm(forms.Form):
-    
+    course = forms.ModelChoiceField(queryset=Course.objects.all(),
+                                    widge=forms.HiddenInput)
--- students/views.py
+++ students/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 
