0ddabd0643fdecaefc65c183f524f72a83854b7c
spadeninja <quanshuli.dev@gmail.com>
2021-10-06 09:08:16 +1000

    Update views.py

--- students/views.py
+++ students/views.py
@@ -4,8 +4,8 @@ from django.views.generic.edit import CreateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from .forms import CourseEnrollForm
 
-# Create your views here.
 class StudentRegistrationView(CreateView):
     template_name = 'students/student/registration.html'
     form_class = UserCreationForm
@@ -18,3 +18,16 @@ class StudentRegistrationView(CreateView):
                             password=cd['password'])
         login(self.request, user)
         return result 
+
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+    course = None 
+    form_class = CourseEnrollForm
+
+    def form_valid(self, form):
+        self.course = form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('student_course_detail',
+                            args=[self.course.id])
