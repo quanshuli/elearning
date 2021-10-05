f36a0c8edc84d17bfd9ff2add0dae0cb22dfd2d1
spadeninja <quanshuli.dev@gmail.com>
2021-10-06 09:44:54 +1000

    Update views.py

--- students/views.py
+++ students/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from .forms import CourseEnrollForm
 from courses.models import Course
+from django.views.generic.detail import DetailView
 
 class StudentRegistrationView(CreateView):
     template_name = 'students/student/registration.html'
@@ -41,3 +42,21 @@ class StudentCourseListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(students__in=[self.request.user])
+
+class StudentCourseDetailView(DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            context['module'] = course.modules.get(
+                                    id=self.kwargs['module_id'])
+        else:
+            context['module'] = course.modules.all()[0]
+        return context 
