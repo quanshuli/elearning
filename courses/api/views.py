0452d227bf58bb4097a415361e08b701a71b0a71
spadeninja <quanshuli.dev@gmail.com>
2021-10-05 13:59:22 +1000

    Update views.py

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404                          
-from rest_framework import generics, viewsets
+from rest_framework import generics, serializers, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response 
 from rest_framework.authentication import BasicAuthentication
@@ -7,6 +7,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action, authentication_classes, permission_classes
 from ..models import Subject, Course
 from .serializers import SubjectSerializer, CourseSerializer
+from .permissions import IsEnrolled
+from .serializers import CourseWithContentsSerializer
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
@@ -16,11 +18,17 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
             methods=['post'],
             authentication_classes=[BasicAuthentication],
             permission_classes=[IsAuthenticated])
-            
     def enroll(self, request, *args, **kwargs):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @action(detail=True,
+            methods=['get'],
+            serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
