98bf2abd89199331c2fe169f52a3c57672e97a84
spadeninja <quanshuli.dev@gmail.com>
2021-10-05 13:55:34 +1000

    Update serializers.py

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -25,5 +25,22 @@ class ItemRelatedField(serializers.ReadOnlyField):
 class ContentSerializer(serializers.ModelSerializer):
     item = ItemRelatedField(read_only=True)
     class Meta:
-        model = Coutent
+        model = Content
         fields = ['order', 'item']
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True) 
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
+
+    class Meta:
+        model = Course 
+        fields = ['id', 'subject', 'title', 'slug',
+                  'overview', 'created', 'owner', 'modules']
+
+    
